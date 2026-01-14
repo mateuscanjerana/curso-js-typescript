e45d4cb561f76d19ecd18db27e93dcaba9492e4d
Mateus Canjerana <88593542+MateusCanjerana@users.noreply.github.com>
2026-01-14 18:14:15 -0300

    refactor: update comments for clarity and add new HTML and JS files

--- aula4/comentarios.js
+++ aula4/comentarios.js
@@ -1,6 +1,6 @@
 //Comentários são texto de codigos que são ignorados pelo motor do JavaScript
 console.log("Olá, mundo!"); //Aqui tem outro comentário
-//comentario 
+//Esse é um comentário
 console.log('Linha 1');
 //console.log('Linha 2');
 
@@ -8,6 +8,9 @@ console.log('Linha 1');
     Esse é um bloco de comentário 
     vai ser ignorado
 */
+/*
    console.log('Linha 3');
-//linha de comentário
+   console.log('Linha 4');
+*/
+// Vai ser ignorado
 
---
+++ aula5/js/index.js
@@ -0,0 +1,13 @@
+//É uma boa prática colocar a TAG <script> no final da TAG <body> para evitar demora no carregamento 
+console.log("Olá mundo!");
+console.log("Este trecho será exibido no console do navegador!");
+alert("Olá, mundo!");
+
+
+// Terminal
+/*
+PS C:\Users\Mateu\Documents\GitHub\curso-js-typescript> cd aula5
+PS C:\Users\Mateu\Documents\GitHub\curso-js-typescript\aula5> node index.js
+
+PS C:\Users\Mateu\Documents\GitHub\curso-js-typescript> node aula5/index.js
+*/
