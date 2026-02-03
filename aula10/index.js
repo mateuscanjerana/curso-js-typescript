0162bae6f58e9c37130d09cdcdcd3f38655e4842
Mateus Canjerana <88593542+MateusCanjerana@users.noreply.github.com>
2026-02-03 17:50:54 -0300

    var & tipos de dados
    Atualização dos estudos sobre diferenças de VAR para let, const + tipos de dados primitivos

---
+++ aula10/index.js
@@ -0,0 +1,21 @@
+//Tipos de dados primitivo em JavaScript
+
+//String = texto
+const nome1 = "João";
+const nome2 = 'Maria';
+
+//Number = inteiro e decimal
+const num1 = 25;
+const num2 = 70.5;
+
+let nomeAluno; //Undefined = não aponta para local nenhum na memória - usado para declarar uma variável sem valor
+let sobrenomeAluno = null; //Nulo = não aponta para local nenhum na memória - usado para zerar um valor 
+
+//Boolean = true ou false
+const aprovado = true; //Valor logico
+const reprovado = false;
+
+console.log(typeof nome1); //Usado para saber o tipo de dado
+
+
+
---
+++ aula9/index.js
@@ -0,0 +1,11 @@
+// Diferença entre VAR e LET/CONST
+
+// VAR
+var nome = 'João';
+console.log(nome); // João
+nome = 'Mateus'; //Não faça isso (sem var, let ou const)
+var nome = 'Luiz';
+var nome = 'Otavio'; // Redeclaração permitida com var mas não com let ou const não é boa prática
+console.log(nome); // Otavio
+
+
